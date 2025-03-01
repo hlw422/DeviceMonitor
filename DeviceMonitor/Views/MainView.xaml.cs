a6aa431328557b3322624446cfe650624b63e821
hlw422 <hlw422@126.com>
2025-03-01 15:10:22 +0800

    1.增加iconFont字体图标，一个字体文件可以包含多个图标 2.增加头部的两个梯形按钮。 3.Window.Resources 全局配置 4.ContentPresenter 原样展示原始内容 5.ControlTemplate 控件样式 6.TextBlock.RenderTransform 水平拉伸

--- DeviceMonitor/Views/MainView.xaml.cs
+++ DeviceMonitor/Views/MainView.xaml.cs
@@ -18,7 +18,7 @@ namespace DeviceMonitor.Views
     /// MainView.xaml 的交互逻辑
     /// </summary>
     public partial class MainView : Window
-    {
+    { 
         public MainView()
         {
             InitializeComponent();
